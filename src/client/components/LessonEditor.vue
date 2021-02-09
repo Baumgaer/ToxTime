bd5b2a4fbd11d6284ee721111b91443bb936b718
René Baumgärtner <terminator1000000@web.de>
2021-02-09 19:23:46 +0100

    extend lesson editor with DnD

--- src/client/components/LessonEditor.vue
+++ src/client/components/LessonEditor.vue
@@ -4,9 +4,19 @@
         <section class="editorBody">
             <h3>{{ $t("scenes") }}</h3>
             <section class="scenes">
-                <div class="scene" v-for="(scene, index) of model.scenes" :key="index">
+                <div class="scene"
+                     v-for="(scene, index) of model.scenes"
+                     :key="index"
+                     draggable
+                     @dragstart="onDragStart($event, scene)"
+                     @dragend="onDragEnd($event)"
+                     @dragover="onDragOver($event, index)"
+                     @dragleave="onDragLeave($event, index)"
+                     @drop="onInternalDrop($event, index)"
+                     :ref="`scene${index}`"
+                >
                     <div class="scenePicture" :style="`background-image: url(${scene.getAvatar().name})`"></div>
-                    <component :is="'close-icon'" class="closeIcon"/>
+                    <component :is="'close-icon'" class="closeIcon" @click="onSceneRemoveClick(scene)"/>
                     <div class="name">{{ scene.name }}</div>
                 </div>
             </section>
@@ -57,7 +67,11 @@ export default {
             console.log("SAVE!");
         },
 
-        onInternalDrop(event) {
+        /**
+         * @param {DragEvent} event
+         * @param {number} [index]
+         */
+        onInternalDrop(event, index) {
             if (!ApiClient.store.collection("localStorage").isInternalDnD) return;
 
             event.preventDefault();
@@ -68,8 +82,75 @@ export default {
             if (eventData) {
                 const model = ApiClient.store.getModelById(eventData.collection, eventData._id);
                 if (!model || !(model instanceof Scene.RawClass)) return;
-                this.model.scenes.push(model);
+
+                if (typeof index === "number") {
+                    const element = this.$refs[`scene${index}`][0];
+                    const elementRect = element.getBoundingClientRect();
+                    let indexOfDraggedModel = this.model.scenes.indexOf(model);
+
+                    let indexAddition = 0;
+                    if (elementRect.x + elementRect.width / 2 <= event.clientX) indexAddition = 1;
+
+                    element.classList.remove("rightDropTarget");
+                    element.classList.remove("leftDropTarget");
+
+                    if (indexOfDraggedModel >= 0) {
+                        this.model.scenes.splice(indexOfDraggedModel, 1);
+                    } else indexOfDraggedModel = index;
+                    this.model.scenes.splice(indexOfDraggedModel + indexAddition, 0, model);
+                } else this.model.scenes.push(model);
+
             }
+        },
+
+        onSceneRemoveClick(scene) {
+            const index = this.model.scenes.indexOf(scene);
+            if (index < 0) return;
+            this.model.scenes.splice(index, 1);
+        },
+
+        /**
+         * @param {DragEvent} event
+         */
+        onDragStart(event, model) {
+            event.stopPropagation();
+            ApiClient.store.collection("localStorage").isInternalDnD = true;
+            event.dataTransfer.setData("model", JSON.stringify({collection: model.collection, _id: model._id}));
+        },
+
+        onDragEnd() {
+            ApiClient.store.collection("localStorage").isInternalDnD = false;
+        },
+
+        /**
+         * @param {DragEvent} event
+         * @param {number} index
+         */
+        onDragOver(event, index) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            /** @type {HTMLElement} */
+            const element = this.$refs[`scene${index}`][0];
+            const elementRect = element.getBoundingClientRect();
+
+            if (elementRect.x + elementRect.width / 2 > event.clientX) {
+                element.classList.add("leftDropTarget");
+                element.classList.remove("rightDropTarget");
+            } else {
+                element.classList.remove("leftDropTarget");
+                element.classList.add("rightDropTarget");
+            }
+        },
+
+        onDragLeave(event, index) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            /** @type {HTMLElement} */
+            const element = this.$refs[`scene${index}`][0];
+            element.classList.remove("rightDropTarget");
+            element.classList.remove("leftDropTarget");
         }
     }
 };
