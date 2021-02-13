1d518154ee0cd1b4b7f88f83088da2831793c065
René Baumgärtner <terminator1000000@web.de>
2021-02-13 17:15:10 +0100

    add store and logout button

--- src/client/components/Public.vue
+++ src/client/components/Public.vue
@@ -1,13 +1,13 @@
 <template>
     <section class="public">
         <nav class="navigation" ref="navigation">
-            <Button ref="lessons" name="lessons" @click="onNavButtonClick('lessons')">
+            <Button ref="lessons" name="lessons" :active="this.category === 'lessons'" @click="onNavButtonClick('lessons')">
                 <school-icon />
             </Button>
-            <Button ref="settings" name="settings" @click="onNavButtonClick('settings')">
+            <Button ref="settings" name="settings" :active="this.category === 'settings'" @click="onNavButtonClick('settings')">
                 <cog-icon />
             </Button>
-            <Button ref="logout" name="logout" @click="onNavButtonClick('logout')">
+            <Button ref="logout" name="logout" :active="this.category === 'logout'" @click="onLogoutButtonClick()">
                 <logout-icon />
             </Button>
         </nav>
@@ -17,13 +17,27 @@
 
 <script>
 import Button from "~client/components/Button";
+import ApiClient from "~client/lib/ApiClient";
+
 export default {
     components: {
         Button
     },
+    data() {
+        return {
+            store: {},
+            category: "lessons"
+        };
+    },
     methods: {
-        onNavButtonClick(name) {
-            console.log(name);
+        async onNavButtonClick(name) {
+            this.category = name;
+            this.store = ApiClient.store.collection(this.category);
+            await ApiClient.get(`/${name}`);
+        },
+
+        onLogoutButtonClick() {
+            location.href = "/logout";
         }
     }
 };
