dc1b4f1a4bd6c67ec31150af2d7ebff18a98e136
René Baumgärtner <terminator1000000@web.de>
2021-02-15 21:53:25 +0100

    make settings work in public view

--- src/client/components/Public.vue
+++ src/client/components/Public.vue
@@ -4,24 +4,28 @@
             <Button ref="lessons" name="lessons" :active="this.category === 'lessons'" @click="onNavButtonClick('lessons')">
                 <school-icon />
             </Button>
-            <Button ref="settings" name="settings" :active="this.category === 'settings'" @click="window.activeUser.edit()">
+            <Button ref="settings" name="settings" :active="this.category === 'settings'" @click="onSettingsButtonClick()">
                 <cog-icon />
             </Button>
             <Button ref="logout" name="logout" :active="this.category === 'logout'" @click="onLogoutButtonClick()">
                 <logout-icon />
             </Button>
         </nav>
-        <main class="main"></main>
+        <main class="main">
+            <UserEditor v-if="window.activeUser.activeEditor === 'editUser' && category === 'settings'" />
+        </main>
     </section>
 </template>
 
 <script>
 import Button from "~client/components/Button";
+import UserEditor from "~client/components/UserEditor";
 import ApiClient from "~client/lib/ApiClient";
 
 export default {
     components: {
-        Button
+        Button,
+        UserEditor
     },
     data() {
         return {
@@ -34,6 +38,11 @@ export default {
             this.category = name;
             this.store = ApiClient.store.collection(this.category);
             await ApiClient.get(`/${name}`);
+        },
+
+        onSettingsButtonClick() {
+            this.category = "settings";
+            window.activeUser.edit();
         },
 
         onLogoutButtonClick() {
