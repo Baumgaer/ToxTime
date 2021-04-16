40181f150bc0211b74bc2549f590909f592c3ab6
René Baumgärtner <terminator1000000@web.de>
2021-04-16 15:44:14 +0200

    add delete button

--- src/client/models/Recipe.js
+++ src/client/models/Recipe.js
@@ -1,5 +1,6 @@
 import { RecipeMixinClass } from "~common/models/Recipe";
 import ClientModel from "~client/lib/ClientModel";
+import ApiClient from "~client/lib/ApiClient";
 
 const CommonClientRecipe = RecipeMixinClass(ClientModel);
 export default ClientModel.buildClientExport(class Recipe extends CommonClientRecipe {
@@ -19,6 +20,20 @@ export default ClientModel.buildClientExport(class Recipe extends CommonClientRe
             name: "graph-icon",
             title: window.$t("recipe")
         };
+    }
+
+    @CommonClientRecipe.action("delete", { type: "component", name: "delete-icon" }, () => window.activeUser.isAdmin)
+    async delete() {
+        if (!this._id) {
+            this.destroy();
+            window.activeUser.activeEditor = null;
+            window.activeUser.editingModel = null;
+            return;
+        }
+
+        const result = await ApiClient.delete(`/recipes/${this._id}`);
+        if ((result instanceof Error)) return result;
+        ApiClient.store.removeModel(this);
     }
 
     @CommonClientRecipe.action("edit", { type: "component", name: "lead-pencil-icon" }, () => window.activeUser.isAdmin)
