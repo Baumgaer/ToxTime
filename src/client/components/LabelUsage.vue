381e71200ea9a2941f6ffa0f21b89e851c1b1bf8
René Baumgärtner <terminator1000000@web.de>
2021-04-25 17:46:26 +0200

    translate label usage and add inheritance

--- src/client/components/LabelUsage.vue
+++ src/client/components/LabelUsage.vue
@@ -36,9 +36,18 @@
             öffnet sich ein Auswahlfeld mit einer Suche. Suchen Sie Ihr
             gewünschtes Label und klicken Sie dieses Label anschließend an.
             Das Auswahlfeld schließt sich automatisch und das Label wird im Popup angezeigt.
+
+            <h3>4 Vererbung von Label</h3>
+            Label werden automatisch von geerbt. Wenn beispielsweise ein
+            Szenenobjekt eine Bilddatei nutzt, die das Label "Hammer" trägt,
+            dann trägt dieses Szenenobjekt dieses Label ebenfalls. Das gilt
+            auch dann, wenn das Label erst nach dem Szenenobjekt erstellt und
+            der Datei zugewiesen wurde. Label, die geerbt wurden, können nicht
+            auf dem Objekt entfernt werden und werden leicht gräulich dargestellt.
+            Es können nur Label von Dateien geerbt werden.
         </div>
         <div v-else>
-            <h3> 1 General </h3>
+            <h3>1 General</h3>
             Labels are used to generalize objects that basically contain the
             same content with minor modifications. Example:<br />
             <br />
@@ -52,7 +61,7 @@
             It doesn't matter which hammer is in the inventory or in the player's hand.
             The recipe then works with any hammer.
 
-            <h3> 2 Add new label </h3>
+            <h3>2 Add new label </h3>
             To add a new label, click on the plus symbol <plus-icon />, while
             you are in the "Label" category <label-multiple-icon /> (the current category),
             at the top of the list or on the button "Add Article" at the bottom of the list.<br />
@@ -61,7 +70,7 @@
             You can then rename this label. However, labels must have a unique name,
             otherwise they cannot be distinguished from one another.
 
-            <h3> Assign 3 labels </h3>
+            <h3>3 Assign 3 labels</h3>
             To assign a label to an object, move the mouse over the object in
             the list. A popup will now open on the right-hand side of the listing.
             In this popup there is a circled plus symbol <plus-icon />.
@@ -69,6 +78,15 @@
             Find your desired label and then click on this label.
             The selection field closes automatically and the label is
             displayed in the popup.
+
+            <h3>4 Label inheritance</h3>
+            Labels are automatically inherited from. For example, if a scene
+            object uses an image file that carries the label "hammer", then
+            that scene object also carries that label. This is true even if the
+            label was created and assigned to the file after the scene object.
+            Labels that have been inherited cannot be removed on the object and
+            are displayed in a slightly grayish color. Only labels of files can
+            be inherited.
         </div>
     </div>
 </template>
